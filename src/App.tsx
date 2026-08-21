8d29ab3ef8ca364d690de87b8c4e7d139f232f29
huxlic <oladimejihassan03@gmail.com>
2026-08-21 12:44:29 +0100

    Update App layout height and enhance scrollbar styles in CSS

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ const App = () => {
 	return (
 		<>
 			<div
-				className={"font-work-sans text-warm-cream selection:bg-ember-accent h-screen bg-amber-400"}>
+				className={"w-full font-work-sans text-warm-cream selection:bg-ember-accent h-[500vh]"}>
 				<Navbar/>
 			</div>
 		</>
