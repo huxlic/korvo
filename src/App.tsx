18673c8f38436b853eb18fde05ba8e9f3dc791d6
huxlic <oladimejihassan03@gmail.com>
2026-08-21 09:49:29 +0100

    Enhance Navbar layout with new styles and add Wordmark component

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ const App = () => {
 	return (
 		<>
 			<div
-				className={"font-work-sans text-warm-cream selection:bg-ember-accent"}>
+				className={"font-work-sans text-warm-cream selection:bg-ember-accent h-screen bg-amber-400"}>
 				<Navbar/>
 			</div>
 		</>
--- src/components/sections/Navbar.tsx
+++ src/components/sections/Navbar.tsx
@@ -1,13 +1,24 @@
+import {Wordmark} from "../ui/Wordmark.tsx";
 import navLinks from "../../data/navLinks.ts";
 
 export const Navbar = () => {
 	return (
 		<>
-			<header>
-				<nav>
-					<ul>
+			<header className={"px-10 py-6"}>
+				
+				<nav className={"flex justify-between items-center box-border"}>
+					<div className="relative">
+						<div className="w-2.5 h-2.5 rounded-full bg-warm-cream"/>
+						<div className="absolute top flex flex-col items-end gap-2">
+							<p className={"uppercase font-semibold tracking-tighter"}>Made for mugs. Built for tables.</p>
+							<a href="#hero">
+								<Wordmark className={"w-137.5"}/>
+							</a>
+						</div>
+					</div>
+					<ul className={"flex gap-4 text-[11px] font-semibold"}>
 						{navLinks.map(({label, href}) => (
-							<li key={href} className={"uppercase"}>
+							<li key={href} className={"uppercase scale-y-105 border-b-[1.5px] border-dashed"}>
 								<a href={href}>{label}</a>
 							</li>
 						))}
