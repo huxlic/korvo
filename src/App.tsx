7818fb832ec28aa8c7bc777e87a3d7b2bd1001fa
huxlic <oladimejihassan03@gmail.com>
2026-08-22 12:33:40 +0100

    improve gsap animations

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,42 @@
 import {Navbar} from "./components/sections/Navbar.tsx";
 import {Hero} from "./components/sections/Hero.tsx";
+// import {useGSAP} from "@gsap/react";
+import {ScrollTrigger} from "gsap/ScrollTrigger";
+import {ScrollSmoother} from "gsap/ScrollSmoother";
+import gsap from "gsap";
+
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 
 const App = () => {
+	// useGSAP(() => {
+	// 	const smoother = ScrollSmoother.create({
+	// 		wrapper: "#smooth-wrapper",
+	// 		content: "#smooth-content",
+	// 		smooth: 2,
+	// 		effects: true,
+	// 	});
+	//
+	// 	// Force the smoother to sync its internal scroll position immediately
+	// 	smoother.scrollTop(smoother.scrollTop());
+	//
+	// 	requestAnimationFrame(() => {
+	// 		ScrollTrigger.refresh();
+	// 	});
+	//
+	// 	return () => smoother?.kill();
+	// }, []);
+	
 	return (
-		<>
+		<div className="relative w-full font-work-sans text-warm-cream selection:bg-ember-accent">
+			<Navbar />
 			<div
-				className={"w-full h-[500vh] font-work-sans text-warm-cream selection:bg-ember-accent"}>
-				<Navbar/>
-				<Hero/>
+				id="smooth-wrapper"
+			>
+				<div id="smooth-content" className="w-full h-[500vh]">
+					<Hero />
+				</div>
 			</div>
-		</>
-	)
-}
+		</div>
+	);
+};
 export default App
--- src/components/sections/Navbar.tsx
+++ src/components/sections/Navbar.tsx
@@ -3,14 +3,10 @@ import navLinks from "../../data/navLinks.ts";
 import {FlipLink} from "../ui/FlipLink.tsx";
 import {gsap} from "gsap";
 import {SplitText} from "gsap/SplitText";
-import {ScrollTrigger} from "gsap/ScrollTrigger";
-import {Flip} from "gsap/Flip";
 import {useGSAP} from "@gsap/react";
 import {useRef} from "react";
 
 gsap.registerPlugin(SplitText);
-gsap.registerPlugin(ScrollTrigger);
-gsap.registerPlugin(Flip);
 
 export const Navbar = () => {
 	const wordmarkRef = useRef(null);
@@ -31,16 +27,16 @@ export const Navbar = () => {
 					trigger: "#hero",
 					start: "top top",
 					end: "+=500",
-					scrub: 0.5,
+					scrub: true
 				},
-				ease: "power2.inOut",
+				
 			},
 		});
 		
 		tl.to(split.words, {
 				yPercent: -100,
-				duration: 0.25,
-				stagger: 0.01,
+				stagger: 0.05,
+				duration: 1,
 			})
 			.to(
 				wordmark,
@@ -49,20 +45,20 @@ export const Navbar = () => {
 					left: 0,
 					top: "50%",
 					yPercent: -50,
-					duration: 0.1
+					duration: 1,
+					ease: "power2.inOut",
 				}
 			)
 			.to(dot,
-			{
-				width: 0,
-				height: 0,
-			});
+				{
+					width: 0,
+					height: 0,
+				});
 	}, {});
 	
 	return (
 		<>
 			<header className={"px-10 py-6 fixed top-0 w-full z-50"}>
-				
 				<nav className={"flex justify-between items-center box-border"}>
 					<div className="relative w-138">
 						<div className="w-2.5 h-2.5 rounded-full bg-warm-cream" ref={dotRef}/>
@@ -72,7 +68,7 @@ export const Navbar = () => {
 							mugs. Built for tables.</p>
 						
 						<a ref={wordmarkRef} className={"absolute top-10 w-137.5"} href="#hero">
-							<Wordmark className={""}/>
+							<Wordmark />
 						</a>
 					</div>
 					<div className={"flex gap-4 text-[10px] font-semibold"}>
