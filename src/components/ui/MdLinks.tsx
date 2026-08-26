fa6c83a9632cfcb7c22e86e80ce5aa445d001248
huxlic <oladimejihassan03@gmail.com>
2026-08-26 18:51:52 +0100

    Enhance mobile navigation with Zustand state management and improve layout of MdLinks component

--- src/components/ui/MdLinks.tsx
+++ src/components/ui/MdLinks.tsx
@@ -1,20 +1,32 @@
 import navLinks from "../../data/navLinks.ts";
+import useOpenMenu from "../../store/store.ts";
 
 export const MdLinks = () => {
+	const {isOpen, setIsOpen} = useOpenMenu();
+	
 	return (
-		<div className={"md:hidden grid fixed z-49 inset-0"}>
-			<div className="bg-walnut-shadow">
-				<ul className={"flex flex-col gap-2"}>
+		<div className={"md:hidden grid grid-cols-4 fixed z-60 inset-0 bg-[#201914bf]"}>
+			<div className="flex flex-col justify-between bg-walnut-shadow col-span-3 col-start-2 px-10 py-8">
+				<div/>
+				<div className={"flex flex-col gap-6 uppercase text-[73px] font-semibold tracking-tighter"}>
 					{
 						navLinks.map(({label, href}) => (
-							<li key={label}>
+							<li className={"relative w-max list-none leading-none"} onClick={() => setIsOpen(!isOpen)}
+							    key={label}>
 								<a href={href}>
 									{label}
 								</a>
+								<span className="absolute left-0 -bottom-2 w-full h-px dashline"/>
 							</li>
 						))
 					}
-				</ul>
+				</div>
+				
+				<div className="uppercase font-semibold">
+					<span
+						className={"text-driftwood text-[24px] tracking-tighter"}>general enquires:</span>
+					<p className={"text-[32px] tracking-tighter scale-y-105"}>oladimejihassan03@gmail.com</p>
+				</div>
 			</div>
 		</div>
 	);
