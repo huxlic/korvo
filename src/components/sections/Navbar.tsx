a77855431c17f42e4b03c658ffade3317b1020bc
huxlic <oladimejihassan03@gmail.com>
2026-08-27 11:52:21 +0100

    Enhance Navbar component with layout adjustments for improved responsiveness on smaller screens

--- src/components/sections/Navbar.tsx
+++ src/components/sections/Navbar.tsx
@@ -76,8 +76,8 @@ export const Navbar = () => {
 				<nav className={"relative w-full flex justify-between items-center box-border"}>
 					
 					<div
-						className="flex flex-col items-end sm:block left-0 sm:relative z-60 w-full :w-[clamp(326px,45vw,550px)]">
-						<div className="hidden md:block w-2 h-2 lg:w-2.5 lg:h-2.5 rounded-full bg-warm-cream"
+						className="flex flex-col items-end sm:block left-0 pt-16 sm:pt-0 sm:top-4 md:top-0 sm:relative z-60 w-full sm:w-[clamp(326px,45vw,550px)]">
+						<div className="hidden sm:block w-2 h-2 lg:w-2.5 lg:h-2.5 rounded-full bg-warm-cream"
 						     ref={dotRef}/>
 						<p className={"text text-[clamp(10px,3vw,24px)] sm:text-[10px] md:text-[13px] lg:text-[16px] sm:absolute right-0 top-full w-max uppercase font-semibold tracking-tighter overflow-hidden "}
 						   aria-label={"Trained on stains. Deployed on tables."}>Trained on stains. Deployed on tables.
@@ -103,7 +103,7 @@ export const Navbar = () => {
 			</header>
 			<button
 				onClick={() => setIsOpen(!isOpen)}
-				className={`fixed top-4 lg:top-6 right-8 lg:right-10 z-70 flex items-center gap-2 ${isOpen ? "bg-warm-cream text-walnut-shadow border-warm-cream" : "border-dotted"} md:hidden px-[1em] py-[0.4em] xs:px-[1.5em] xs:py-[0.6em] md:px-[1.8em] md:py-[0.9em] border-[1.4px] rounded-full cursor-pointer transition-all duration-200`}
+				className={`fixed top-4 lg:top-6 right-8 lg:right-10 z-70 flex items-center gap-2 ${isOpen ? "bg-warm-cream text-walnut-shadow border-warm-cream" : "border-dotted"} md:hidden px-[1em] py-[0.4em] xs:px-[1.5em] xs:py-[0.6em] sm:px-[1.8em] sm:py-[0.9em] border-[1.4px] rounded-full cursor-pointer transition-all duration-200`}
 			>
 				<div
 					className={`w-2 h-2 lg:w-2.5 lg:h-2.5 rounded-full ${isOpen ? "bg-walnut-shadow" : "bg-warm-cream"}`}/>
