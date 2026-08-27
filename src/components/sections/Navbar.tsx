dcf77c686b2b0150500b8f6718a289368c01268c
huxlic <oladimejihassan03@gmail.com>
2026-08-27 13:08:03 +0100

    Enhance MdLinks and Navbar components with layout adjustments and GSAP animations for improved user experience

--- src/components/sections/Navbar.tsx
+++ src/components/sections/Navbar.tsx
@@ -72,7 +72,7 @@ export const Navbar = () => {
 	
 	return (
 		<>
-			<header className={"px-6 md:px-8 lg:px-10 py-0 md:py-4 lg:py-6 fixed top-0 w-full z-50"}>
+			<header className={"px-4 md:px-8 lg:px-10 py-0 md:py-4 lg:py-6 fixed top-0 w-full z-50"}>
 				<nav className={"relative w-full flex justify-between items-center box-border"}>
 					
 					<div
@@ -103,11 +103,11 @@ export const Navbar = () => {
 			</header>
 			<button
 				onClick={() => setIsOpen(!isOpen)}
-				className={`fixed top-4 lg:top-6 right-8 lg:right-10 z-70 flex items-center gap-2 ${isOpen ? "bg-warm-cream text-walnut-shadow border-warm-cream" : "border-dotted"} md:hidden px-[1em] py-[0.4em] xs:px-[1.5em] xs:py-[0.6em] sm:px-[1.8em] sm:py-[0.9em] border-[1.4px] rounded-full cursor-pointer transition-all duration-200`}
+				className={`fixed top-4 lg:top-6 right-4 md:right-8 lg:right-10 z-70 flex items-center gap-2 ${isOpen ? "bg-warm-cream text-walnut-shadow border-warm-cream" : "border-dotted"} md:hidden px-[1em] py-[0.4em] xs:px-[1.5em] xs:py-[0.6em] sm:px-[1.8em] sm:py-[0.9em] border-[1.4px] rounded-full cursor-pointer transition-all duration-200`}
 			>
 				<div
 					className={`w-2 h-2 lg:w-2.5 lg:h-2.5 rounded-full ${isOpen ? "bg-walnut-shadow" : "bg-warm-cream"}`}/>
-				<p className={"uppercase text-[12px] xs:text-[14px] md:text-[20px] font-semibold tracking-tighter scale-y-105"}>
+				<p className={"uppercase text-[12px] xs:text-[14px] sm:text-[20px] font-semibold tracking-tighter scale-y-105"}>
 					{isOpen ? "Close" : "Menu"}
 				</p>
 			</button>
--- src/components/ui/MdLinks.tsx
+++ src/components/ui/MdLinks.tsx
@@ -1,13 +1,25 @@
 import navLinks from "../../data/navLinks.ts";
 import useOpenMenu from "../../store/store.ts";
+import {useGSAP} from "@gsap/react";
+import gsap from "gsap";
 
 export const MdLinks = () => {
 	const {isOpen, setIsOpen} = useOpenMenu();
 	
+	useGSAP(() => {
+		gsap.from("#md-links", {
+			xPercent: 100,
+		})
+	}, {})
+	
+	// const revealTween = gsap.from("#md-links", {
+	// 	xPercent: 100,
+	// 	paused: true,
+	// });
 	return (
 		<div className={"md:hidden grid grid-cols-4 fixed z-60 inset-0 bg-[#201914bf]"}>
 			<div onClick={() => setIsOpen(!isOpen)}/>
-			<div className="flex flex-col justify-between bg-walnut-shadow col-span-3 px-10 py-8">
+			<div id="md-links" className="flex flex-col justify-between bg-walnut-shadow col-span-3 px-10 py-8">
 				<div/>
 				<div className={"flex flex-col gap-6 uppercase text-[73px] font-semibold tracking-tighter"}>
 					{
@@ -23,10 +35,12 @@ export const MdLinks = () => {
 					}
 				</div>
 				
-				<div className="uppercase font-semibold">
+				<div className="flex flex-col uppercase font-semibold">
 					<span
-						className={"text-driftwood text-[24px] tracking-tighter"}>general enquires:</span>
-					<p className={"text-[32px] tracking-tighter scale-y-105"}>oladimejihassan03@gmail.com</p>
+						className={"w-max text-driftwood text-[22px] tracking-tighter"}>general enquires:</span>
+					
+					<a href="mailto:oladimejihassan03@gmail.com"
+					   className={"text-[28px] tracking-tighter scale-y-105 leading-none"}>oladimejihassan03@gmail.com</a>
 				</div>
 			</div>
 		</div>
