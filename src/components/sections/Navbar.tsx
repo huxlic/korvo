baf4859d6e8464c240db644eec48c6fef072aec5
huxlic <oladimejihassan03@gmail.com>
2026-08-27 11:34:22 +0100

    Enhance Navbar component with responsive design adjustments for smaller screens-

--- src/components/sections/Navbar.tsx
+++ src/components/sections/Navbar.tsx
@@ -68,15 +68,15 @@ export const Navbar = () => {
 					width: 0,
 					height: 0,
 				});
-	}, {});
+	}, {dependencies: [isSmDown, isMdDown]});
 	
 	return (
 		<>
-			<header className={"px-8 lg:px-10 py-4 lg:py-6 fixed top-0 w-full z-50"}>
+			<header className={"px-6 md:px-8 lg:px-10 py-0 md:py-4 lg:py-6 fixed top-0 w-full z-50"}>
 				<nav className={"relative w-full flex justify-between items-center box-border"}>
 					
 					<div
-						className="flex flex-col items-end sm:block left-0 sm:p-0 sm:relative z-60 w-full sm:w-[clamp(326px,45vw,550px)]">
+						className="flex flex-col items-end sm:block left-0 sm:relative z-60 w-full :w-[clamp(326px,45vw,550px)]">
 						<div className="hidden md:block w-2 h-2 lg:w-2.5 lg:h-2.5 rounded-full bg-warm-cream"
 						     ref={dotRef}/>
 						<p className={"text text-[clamp(10px,3vw,24px)] sm:text-[10px] md:text-[13px] lg:text-[16px] sm:absolute right-0 top-full w-max uppercase font-semibold tracking-tighter overflow-hidden "}
@@ -103,11 +103,11 @@ export const Navbar = () => {
 			</header>
 			<button
 				onClick={() => setIsOpen(!isOpen)}
-				className={`fixed top-4 lg:top-6 right-8 lg:right-10 z-70 flex items-center gap-2 ${isOpen ? "bg-warm-cream text-walnut-shadow border-warm-cream" : "border-dotted"} md:hidden px-[1.8em] py-[0.9em] border-[1.4px] rounded-full cursor-pointer transition-all duration-200`}
+				className={`fixed top-4 lg:top-6 right-8 lg:right-10 z-70 flex items-center gap-2 ${isOpen ? "bg-warm-cream text-walnut-shadow border-warm-cream" : "border-dotted"} md:hidden px-[1em] py-[0.4em] xs:px-[1.5em] xs:py-[0.6em] md:px-[1.8em] md:py-[0.9em] border-[1.4px] rounded-full cursor-pointer transition-all duration-200`}
 			>
 				<div
 					className={`w-2 h-2 lg:w-2.5 lg:h-2.5 rounded-full ${isOpen ? "bg-walnut-shadow" : "bg-warm-cream"}`}/>
-				<p className={"uppercase text-[20px] font-semibold tracking-tighter scale-y-105"}>
+				<p className={"uppercase text-[12px] xs:text-[14px] md:text-[20px] font-semibold tracking-tighter scale-y-105"}>
 					{isOpen ? "Close" : "Menu"}
 				</p>
 			</button>
